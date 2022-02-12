b3335d80be9697df6674a46fbd80852072fc2792
SLdLok <sfs171052@sfxs.edu.hk>
2022-02-12 23:31:16 +0800

    2 files modified

--- auth.js
+++ auth.js
@@ -5,26 +5,34 @@ var db = firebase.firestore();
 
 function SignIn() {
   const provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    var credential = result.credential;
-    var token = credential.accessToken;
-    var user = result.user;
-    console.log("LogIn already")
-  }).catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    console.log("ERROR: " + error)
-  });
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      var credential = result.credential;
+      var token = credential.accessToken;
+      user = result.user;
+      console.log('LogIn already');
+      sessionStorage.setItem('loginUser', JSON.stringify(user));
+      window.location.href = './student/panel.html';
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      console.log('ERROR: ' + error);
+    });
 }
 
 function SignOut() {
-  firebase.auth().signOut().then(() => {
-    console.log("Logout already")
-  }).catch((error) => {
-    console.log("ERROR: " + error)
-  });
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      console.log('Logout already');
+    })
+    .catch((error) => {
+      console.log('ERROR: ' + error);
+    });
 }
